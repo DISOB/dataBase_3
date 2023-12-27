70c8c93e930364e58405edcff934abf18ab48a9b
DISOB <105884221+DISOB@users.noreply.github.com>
2023-12-27 18:07:50 +0300

    Add files via upload

---
+++ profiler.py
@@ -0,0 +1,20 @@
+import time
+from stats import get_stats
+
+
+# функция декоратор
+def measure_n_times_with_stats(n: int):
+    def inner_func(func):
+        def wrapper(*args, **kwargs):
+            data = []
+            for _ in range(n):
+                start = time.time()
+                func(*args, **kwargs)
+                end = time.time()
+                res = (end-start)*1000
+                data.append(res)
+            [driver, query_type] = (str(func.__name__)).split('_')
+            stats = get_stats(data, query_type, driver)
+            return stats
+        return wrapper
+    return inner_func
---
+++ stats.py
@@ -0,0 +1,31 @@
+from typing import List # подсчёт статистики
+import pandas as pd
+
+driver_k = 'driver'
+type_k = 'query_type'
+min_k = 'min'
+max_k = 'max'
+mean_k = 'mean'
+
+
+def get_stats(data: List[float], query_type: str, driver: str): # подсчитывает максимальное и медлианное время выполнения .
+    data.sort()
+    min_ = data[0]
+    max_ = data[len(data) - 1]
+    mean = data[len(data) // 2]
+    stats = {
+        driver_k: driver,
+        type_k: query_type,
+        min_k: min_,
+        max_k: max_,
+        mean_k: mean
+    }
+    return stats
+
+
+def init_dataframe():
+    return pd.DataFrame(columns=['driver', 'query_type', 'min', 'max', 'mean'])
+
+
+def save_2_file(df: pd.DataFrame, path: str):
+    df.to_csv(index=False, path_or_buf=path)
